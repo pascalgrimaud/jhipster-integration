d5ee110912a85d04812553a00306b01186c20efc
pGrimaud <pascalgrimaud@gmail.com>
2015-10-11 20:29:25 +0200

    Update directory

--- scripts/02-generate-entities.sh
+++ scripts/02-generate-entities.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 cd $TRAVIS_BUILD_DIR/$JHIPSTER
-if [ $JHIPSTER == "sample-app" ] || [ $JHIPSTER == "sample-app-postgresql" ] || [ $JHIPSTER == "sample-app-mongodb" ] || [ $JHIPSTER == "sample-app-elasticsearch" ]; then
+if [ $JHIPSTER == "samples/sample-app" ] || [ $JHIPSTER == "samples/sample-app-postgresql" ] || [ $JHIPSTER == "samples/sample-app-mongodb" ] || [ $JHIPSTER == "samples/sample-app-elasticsearch" ]; then
   yo jhipster:entity BankAccount --force --no-insight
-  if [ $JHIPSTER != "sample-app-mongodb" ]; then
+  if [ $JHIPSTER != "samples/sample-app-mongodb" ]; then
     yo jhipster:entity Label --force --no-insight
     yo jhipster:entity Operation --force --no-insight
   fi
--- scripts/03-docker-compose.sh
+++ scripts/03-docker-compose.sh
@@ -2,11 +2,11 @@
 set -ev
 cd $TRAVIS_BUILD_DIR/$JHIPSTER
 echo '  privileged: true' >> $TRAVIS_BUILD_DIR/$JHIPSTER/docker-compose-prod.yml
-if [ $JHIPSTER == "sample-app-cassandra" ]; then
+if [ $JHIPSTER == "samples/sample-app-cassandra" ]; then
   docker-compose -f docker-compose-prod.yml build
 fi
 docker-compose -f docker-compose-prod.yml up -d
 sleep 20
-if [ $JHIPSTER == "sample-app-cassandra" ]; then
+if [ $JHIPSTER == "samples/sample-app-cassandra" ]; then
   docker exec -it sampleCassandra-prod-cassandra init
 fi
