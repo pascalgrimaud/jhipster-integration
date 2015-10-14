7883b18ee6510565e70392e9a833b42ca60b7d31
pGrimaud <pascalgrimaud@gmail.com>
2015-10-14 21:49:42 +0200

    Change timing

--- scripts/04-run-dev-profile.sh
+++ scripts/04-run-dev-profile.sh
@@ -3,7 +3,7 @@ set -ev
 cd $TRAVIS_BUILD_DIR/$JHIPSTER
 if [ $JHIPSTER != "samples/sample-app-gradle" ]; then
   mvn &
-  sleep 90
+  sleep 120
   curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
   fuser -k 8080/tcp ; sleep 10
 else
