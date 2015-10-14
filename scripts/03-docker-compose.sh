f8a5d74db1beae3077faea5bc7d99153930c3c27
pGrimaud <pascalgrimaud@gmail.com>
2015-10-14 21:45:45 +0200

    20s for docker compose

--- scripts/03-docker-compose.sh
+++ scripts/03-docker-compose.sh
@@ -6,7 +6,7 @@ if [ $JHIPSTER == "samples/sample-app-cassandra" ]; then
   docker-compose -f docker-compose-prod.yml build
 fi
 docker-compose -f docker-compose-prod.yml up -d
-sleep 10
+sleep 20
 if [ $JHIPSTER == "samples/sample-app-cassandra" ]; then
   docker exec -it sampleCassandra-prod-cassandra init
 fi
