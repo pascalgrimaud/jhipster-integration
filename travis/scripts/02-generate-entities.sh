6dd5c7516bd70bd57c99c4962b2915a9b2fe9fba
pGrimaud <pascalgrimaud@gmail.com>
2015-10-17 00:17:25 +0200

    Fix cassandra : no table

--- travis/scripts/02-generate-entities.sh
+++ travis/scripts/02-generate-entities.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -ev
 cd $JHIPSTER_SAMPLES/$JHIPSTER
-yo jhipster:entity BankAccount --force --no-insight
-if [ $JHIPSTER != "app-mongodb" ] && [ $JHIPSTER != "app-cassandra" ]; then
-  yo jhipster:entity Label --force --no-insight
-  yo jhipster:entity Operation --force --no-insight
+if [ $JHIPSTER != "app-cassandra" ]; then
+  yo jhipster:entity BankAccount --force --no-insight
+  if [ $JHIPSTER != "app-mongodb" ]; then
+    yo jhipster:entity Label --force --no-insight
+    yo jhipster:entity Operation --force --no-insight
+  fi
 fi
