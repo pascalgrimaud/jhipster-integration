2384d15e1009ddcf41b8dae44d46367a88e580d0
pGrimaud <pascalgrimaud@gmail.com>
2015-10-17 19:55:56 +0200

    Speed up build

--- travis/install/04-downloadMvnDep.sh
+++ travis/install/04-downloadMvnDep.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -ev
 if [ $JHIPSTER_MVN_DEP == 1 ]; then
-  mv -R $JHIPSTER_TRAVIS/m2/repository/ .m2/
+  docker pull jdubois/jhipster-docker
+  docker run --privileged -d -p 4022:22 --name=jhipster-docker -t jdubois/jhipster-docker
+  docker cp jhipster-docker:/home/jhipster/.m2/repository/ $TRAVIS_BUILD_DIR/.m2/
+  docker cp jhipster-docker:/home/jhipster/jhipster-sample-app/node_modules/ $JHIPSTER_SAMPLES/$JHIPSTER/
+else
+  cd $TRAVIS_BUILD_DIR/
+  git clone https://github.com/jhipster/jhipster-sample-app.git
+  cd $TRAVIS_BUILD_DIR/jhipster-sample-app/ && mvn dependency:go-offline
 fi;
