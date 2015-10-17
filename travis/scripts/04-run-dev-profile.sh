7d221473cf13ac0b7ebc858553ddee43236d8d3f
pGrimaud <pascalgrimaud@gmail.com>
2015-10-17 20:45:57 +0200

    Add 1min to start app

--- travis/scripts/04-run-dev-profile.sh
+++ travis/scripts/04-run-dev-profile.sh
@@ -3,7 +3,7 @@ set -ev
 cd $JHIPSTER_SAMPLES/$JHIPSTER
 if [ $JHIPSTER != "app-gradle" ]; then
   mvn &
-  sleep 120
+  sleep 180
   curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
   fuser -k 8080/tcp ; sleep 10
 else
