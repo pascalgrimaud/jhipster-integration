7918de5b6a5118a2206c32d3de15a638d5ec3176
pGrimaud <pascalgrimaud@gmail.com>
2015-10-17 20:30:39 +0200

    Change m2 directory

--- travis/install/04-downloadMvnDep.sh
+++ travis/install/04-downloadMvnDep.sh
@@ -3,9 +3,10 @@ set -ev
 if [ $JHIPSTER_MVN_DEP == 1 ]; then
   docker pull jdubois/jhipster-docker
   docker run --privileged -d -p 4022:22 --name=jhipster-docker -t jdubois/jhipster-docker
-  docker cp jhipster-docker:/home/jhipster/.m2/repository/ $TRAVIS_BUILD_DIR/.m2/
+  docker cp jhipster-docker:/home/jhipster/.m2/repository/ $HOME/.m2/
   docker cp jhipster-docker:/home/jhipster/jhipster-sample-app/node_modules/ $JHIPSTER_SAMPLES/$JHIPSTER/
-  ls -al $TRAVIS_BUILD_DIR/.m2/repository/
+  ls -al $HOME/.m2/
+  ls -al $HOME/.m2/repository/
 else
   cd $TRAVIS_BUILD_DIR/
   git clone https://github.com/jhipster/jhipster-sample-app.git
