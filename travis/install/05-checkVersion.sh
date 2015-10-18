b6564ddd577def85cf6a37596a5ff6e6034aa86b
pGrimaud <pascalgrimaud@gmail.com>
2015-10-18 21:46:25 +0200

    Add another samples

---
+++ travis/install/05-checkVersion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+java -version
+git --version
+yo --version
+mvn -v
+node -v
+npm -v
+bower -v
+grunt --version
+gulp -v
+grunt --version
+docker version
+docker-compose version
