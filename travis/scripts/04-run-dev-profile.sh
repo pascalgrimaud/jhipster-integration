8b9a60bcda88c28f72a1476bc48eaf0df9e9c1d5
pGrimaud <pascalgrimaud@gmail.com>
2015-10-18 17:27:39 +0200

    Change 5min to Gradle build

--- travis/scripts/04-run-dev-profile.sh
+++ travis/scripts/04-run-dev-profile.sh
@@ -8,7 +8,7 @@ if [ $JHIPSTER != "app-gradle" ]; then
   fuser -k 8080/tcp ; sleep 10
 else
   ./gradlew $JHIPSTER_MODE &
-  sleep 240
+  sleep 300
   curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
   fuser -k 8080/tcp ; sleep 10
 fi
