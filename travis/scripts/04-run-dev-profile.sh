35d8b48e49af0f27f43dcd457152fb1478ef9dc6
pGrimaud <pascalgrimaud@gmail.com>
2015-10-18 09:24:13 +0200

    Change sleep time

--- travis/scripts/04-run-dev-profile.sh
+++ travis/scripts/04-run-dev-profile.sh
@@ -3,12 +3,12 @@ set -ev
 cd $JHIPSTER_SAMPLES/$JHIPSTER
 if [ $JHIPSTER != "app-gradle" ]; then
   mvn &
-  sleep 180
+  sleep 120
   curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
   fuser -k 8080/tcp ; sleep 10
 else
   ./gradlew &
-  sleep 240
+  sleep 180
   curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
   fuser -k 8080/tcp ; sleep 10
 fi
