cf67da7fff5c1c356502f941b5ce167c0b344fd5
pGrimaud <pascalgrimaud@gmail.com>
2015-10-18 17:45:17 +0200

    Display cache

--- travis/install/03-install-jhipster-stack.sh
+++ travis/install/03-install-jhipster-stack.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -ev
+ls -al /usr/lib/node_modules/
 npm install -g yo
 npm install -g bower
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g generator-jhipster
+ls -al /usr/lib/node_modules/
 #--------------------------------------------------
 # use a snapshot version of jhipster
 #--------------------------------------------------
