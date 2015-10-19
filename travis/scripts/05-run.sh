066adab94d8e1b79f24cf2e31273fd0f99ee5725
pGrimaud <pascalgrimaud@gmail.com>
2015-10-20 00:47:45 +0200

    No tests on run tasks

--- travis/scripts/05-run.sh
+++ travis/scripts/05-run.sh
@@ -6,12 +6,12 @@ set -ev
 cd $JHIPSTER_SAMPLES/$JHIPSTER
 if [ $JHIPSTER != "app-gradle" ]; then
   mvn $JHIPSTER_MODE &
-  sleep 240
-  curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
-  fuser -k 8080/tcp ; sleep 10
+  sleep 120
+  # curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
+  # fuser -k 8080/tcp ; sleep 10
 else
   ./gradlew $JHIPSTER_MODE &
   sleep 300
-  curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
-  fuser -k 8080/tcp ; sleep 10
+  # curl --retry 10 --retry-delay 5 -I http://localhost:8080/ | grep "HTTP/1.1 200 OK"
+  # fuser -k 8080/tcp ; sleep 10
 fi
