e3b3e4e6a3ee53326592798d3fa269d819223639
pGrimaud <pascalgrimaud@gmail.com>
2015-10-19 20:30:52 +0200

    Use clone and npm install -g directly

--- travis/install/03-install-jhipster-stack.sh
+++ travis/install/03-install-jhipster-stack.sh
@@ -4,10 +4,9 @@ npm install -g yo
 npm install -g bower
 npm install -g grunt-cli
 npm install -g gulp
-npm install -g generator-jhipster
 #--------------------------------------------------
 # use a snapshot version of jhipster
 #--------------------------------------------------
 git clone https://github.com/jhipster/generator-jhipster.git
 cd $TRAVIS_BUILD_DIR/generator-jhipster
-npm link
+npm install -g
