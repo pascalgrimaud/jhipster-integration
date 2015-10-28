8c1b3363c705ec81518fa39ef0ab929c954243d0
Pascal Grimaud <pascalgrimaud@users.noreply.github.com>
2015-10-28 12:51:41 +0100

    Inverse yo jhipster and npm link

--- travis/scripts/01-generate-project.sh
+++ travis/scripts/01-generate-project.sh
@@ -4,8 +4,8 @@ set -ev
 # Generate the project with yo jhipster
 #--------------------------------------------------
 cd $JHIPSTER_SAMPLES/$JHIPSTER
-yo jhipster --force --no-insight
 npm link generator-jhipster
+yo jhipster --force --no-insight
 ls -al $JHIPSTER_SAMPLES/$JHIPSTER
 ls -al $JHIPSTER_SAMPLES/$JHIPSTER/node_modules/generator-jhipster/
 ls -al $JHIPSTER_SAMPLES/$JHIPSTER/node_modules/generator-jhipster/entity/
