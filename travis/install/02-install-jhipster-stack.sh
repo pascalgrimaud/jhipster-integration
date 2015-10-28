9b997f4ab99a808dba54c7fd903ea4e875d11a64
pGrimaud <pascalgrimaud@gmail.com>
2015-10-28 08:29:00 +0100

    Remove speedup build with docker

--- travis/install/02-install-jhipster-stack.sh
+++ travis/install/02-install-jhipster-stack.sh
@@ -7,6 +7,7 @@ npm install -g gulp
 #--------------------------------------------------
 # use a snapshot version of jhipster
 #--------------------------------------------------
+cd $TRAVIS_BUILD_DIR/
 git clone https://github.com/jhipster/generator-jhipster.git
 cd $TRAVIS_BUILD_DIR/generator-jhipster
 npm install -g
--- travis/install/03-downloadDep.sh
+++ travis/install/03-downloadDep.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 set -ev
 #-------------------------------------------------------------------------------
-# clone official jhipster-sample-app
-#-------------------------------------------------------------------------------
-if [ $JHIPSTER_MVN_DEP == 1 ]; then
-  cd $TRAVIS_BUILD_DIR/
-  git clone https://github.com/jhipster/jhipster-sample-app.git
-  cd $TRAVIS_BUILD_DIR/jhipster-sample-app/
-  mvn dependency:go-offline
-#-------------------------------------------------------------------------------
-# pull official docker image, get .m2 and node_modules
-#-------------------------------------------------------------------------------
-elif [ $JHIPSTER_MVN_DEP == 2 ]; then
-  docker pull jdubois/jhipster-docker
-  docker run --privileged -d -p 4022:22 --name=jhipster-docker -t jdubois/jhipster-docker
-  docker cp jhipster-docker:/home/jhipster/.m2/repository/ $HOME/.m2/
-  docker cp jhipster-docker:/home/jhipster/jhipster-sample-app/node_modules/ $JHIPSTER_SAMPLES/$JHIPSTER/
-  ls -al $HOME/.m2/
-  ls -al $HOME/.m2/repository/
-  ls -al $JHIPSTER_SAMPLES/$JHIPSTER/
-#-------------------------------------------------------------------------------
 # use personal repository that contain .m2 and node_modules
 #-------------------------------------------------------------------------------
-elif [ $JHIPSTER_MVN_DEP == 3 ]; then
+if [ $JHIPSTER_MVN_DEP == 1 ]; then
   cd $TRAVIS_BUILD_DIR/
   git clone https://github.com/pascalgrimaud/jhipster-lib.git
   rm -Rf $HOME/.m2/repository/
@@ -31,6 +12,14 @@ elif [ $JHIPSTER_MVN_DEP == 3 ]; then
   ls -al $HOME/.m2/
   ls -al $HOME/.m2/repository/
   ls -al $JHIPSTER_SAMPLES/$JHIPSTER/
+#-------------------------------------------------------------------------------
+# clone official jhipster-sample-app
+#-------------------------------------------------------------------------------
+elif [ $JHIPSTER_MVN_DEP == 2 ]; then
+  cd $TRAVIS_BUILD_DIR/
+  git clone https://github.com/jhipster/jhipster-sample-app.git
+  cd $TRAVIS_BUILD_DIR/jhipster-sample-app/
+  mvn dependency:go-offline
 #-------------------------------------------------------------------------------
 # By default, do nothing
 #-------------------------------------------------------------------------------
